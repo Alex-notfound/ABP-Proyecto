98a23c59029e3fa296a77b4612e5c150e9b5d39d
Alexandre Currás Rodríguez <alexcurrasr@gmail.com>
2019-11-17 01:54:04 +0100

    Update Enfrentamiento.java

--- padel/src/main/java/com/padelclub/model/Enfrentamiento.java
+++ padel/src/main/java/com/padelclub/model/Enfrentamiento.java
@@ -1,89 +1,77 @@
-//package com.padelclub.model;
-//
-//import java.sql.Date;
-//
-//import javax.persistence.Column;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Id;
-//
-//public class Enfrentamiento {
-//	
-//	@Id
-//	@GeneratedValue(strategy = GenerationType.IDENTITY)
-//	private Long id;
-//	@Column
-//	private UsuarioCampeonatoId pareja11;
-//	@Column
-//	private UsuarioCampeonatoId  pareja12;
-//	@Column
-//	private UsuarioCampeonatoId  pareja21;
-//	@Column
-//	private UsuarioCampeonatoId  pareja22;
-//	@Column
-//	private String resultado;
-//	@Column
-//	private boolean ganador;
-//	@Column
-//	private Reserva2 reserva;
-//	
-//
-//	public Long getId() {
-//		return id;
-//	}
-//	
-//	public void setId(Long id) {
-//		this.id = id;
-//	}
-//	
-//
-//	public UsuarioCampeonatoId getPareja11() {
-//		return pareja11;
-//	}
-//
-//	public void setPareja11(UsuarioCampeonatoId pareja11) {
-//		this.pareja11 = pareja11;
-//	}
-//	
-//	public UsuarioCampeonatoId getPareja12() {
-//		return pareja12;
-//	}
-//
-//	public void setPareja12(UsuarioCampeonatoId pareja12) {
-//		this.pareja12 = pareja12;
-//	}
-//	
-//	public UsuarioCampeonatoId getPareja21() {
-//		return pareja21;
-//	}
-//
-//	public void setPareja21(UsuarioCampeonatoId pareja21) {
-//		this.pareja21 = pareja21;
-//	}
-//	
-//	public UsuarioCampeonatoId getPareja22() {
-//		return pareja22;
-//	}
-//
-//	public void setPareja22(UsuarioCampeonatoId pareja22) {
-//		this.pareja22 = pareja22;
-//	}
-//	
-//	public String getResultado() {
-//		return this.resultado;
-//	}
-//	
-//	public void setResultado(String resultado) {
-//		this.resultado=resultado;
-//	}
-//	
-//	//Si es true, ganó PAREJA1 si es false, PAREJA2
-//	public boolean getGanador() {
-//		return ganador;
-//	}
-//	
-//	public void setGanador(boolean ganador) {
-//		this.ganador=ganador;
-//	}
-//	
-//}
+package com.padelclub.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Enfrentamiento {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@ManyToOne
+	private Pareja pareja1;
+	@ManyToOne
+	private Pareja pareja2;
+	@Column
+	private String resultado;
+	@Column
+	private boolean ganador;
+	@OneToOne
+	private Partido partido;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Pareja getPareja1() {
+		return pareja1;
+	}
+
+	public void setPareja1(Pareja pareja1) {
+		this.pareja1 = pareja1;
+	}
+
+	public Pareja getPareja2() {
+		return pareja2;
+	}
+
+	public void setPareja2(Pareja pareja2) {
+		this.pareja2 = pareja2;
+	}
+
+	public String getResultado() {
+		return resultado;
+	}
+
+	public void setResultado(String resultado) {
+		this.resultado = resultado;
+	}
+
+	public Partido getPartido() {
+		return partido;
+	}
+
+	public void setPartido(Partido partido) {
+		this.partido = partido;
+	}
+
+	// Si es true, ganó PAREJA1 si es false, PAREJA2
+	public boolean getGanador() {
+		return ganador;
+	}
+
+	public void setGanador(boolean ganador) {
+		this.ganador = ganador;
+	}
+
+}
